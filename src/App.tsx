04ed93327be6c6491ab163ffccd34ab53240597b
ross forrester <rossj@macbook-pro-2.home>
2025-09-08 14:18:33 +0100

    reset password

--- src/App.tsx
+++ src/App.tsx
@@ -17,7 +17,6 @@ import SeatManagement from './components/SeatManagement';
 import PrivacyPolicy from './components/PrivacyPolicy';
 import TermsOfService from './components/TermsOfService';
 import ResetPassword from './components/ResetPassword';
-import AuthHandler from './components/AuthHandler';  // ADD THIS LINE
 
 // Create a simple auth context for global state
 interface AuthContextType {
@@ -111,9 +110,6 @@ function AppContent() {
         <Route path="/terms" element={<TermsOfService />} />
         
         <Route path="/privacy" element={<PrivacyPolicy />} />
-
-        {/* Auth Handler for Supabase redirects - ADD THIS LINE */}
-        <Route path="/auth" element={<AuthHandler />} />
 
         {/* Reset Password - accessible without authentication */}
         <Route path="/reset-password" element={<ResetPassword />} />
