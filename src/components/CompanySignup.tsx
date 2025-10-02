2c74146dfcef41e305af6ffa8f1e8a0319769469
ross forrester <rossj@macbook-pro-2.home>
2025-10-02 19:15:27 +0100

    change back brrr

--- src/components/CompanySignup.tsx
+++ src/components/CompanySignup.tsx
@@ -111,7 +111,6 @@ export default function CompanySignup() {
             subscription_type: billingPeriod,
             trial_ends_at: new Date(Date.now() + 60 * 24 * 60 * 60 * 1000).toISOString(),
             role: 'admin',
-            role_type: "admin"
           }
         }
       });
